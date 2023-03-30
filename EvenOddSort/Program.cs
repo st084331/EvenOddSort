96698fde1aa5e28e96cbd58caa3bf9225f3dcbca
st084331 <st084331@student.spbu.ru>
2023-03-30 18:00:35 +0300

    Добавьте файлы проекта.

---
+++ EvenOddSort/Program.cs
@@ -0,0 +1,52 @@
+﻿using MPI;
+using Environment = MPI.Environment;
+
+namespace EvenOddSort;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        using (var env = new Environment(ref args))
+        {
+            int[]? inputArray = null;
+            var commun = Communicator.world;
+
+            if (commun.Rank == 0)
+            {
+            //Console.WriteLine($"{string.Join(" ", args)}");
+
+                if (args.Length < 2)
+                {
+                    return;
+                }
+
+                var inputFile = args[0];
+                var streamReader = new StreamReader(inputFile);
+                inputArray = streamReader.ReadToEnd().Split().Select(x => int.Parse(x)).ToArray();
+                streamReader.Close();
+                inputArray = commun.Scatter(Enumerable.Repeat(inputArray, commun.Size).ToArray(), 0);
+            }
+            else
+            {
+                inputArray = commun.Scatter<int[]>(0);
+            }
+
+            var sorted = Sort.OddEvenSort(inputArray);
+
+            if (commun.Rank == 0)
+            {
+                //Console.WriteLine($"{string.Join(" ", sorted)}");
+                var outputFile = args[1];
+                var streamWriter = new StreamWriter(outputFile);
+                foreach (var elem in sorted)
+                {
+                    streamWriter.Write($"{elem} ");
+                }
+                streamWriter.Write(System.Environment.NewLine);
+                streamWriter.Close();
+            }
+        }
+    }
+}
---
+++ EvenOddSort/Sort.cs
@@ -0,0 +1,65 @@
+﻿using MPI;
+
+namespace EvenOddSort;
+
+public static class Sort
+{
+    private static int[] Merge(int[] arr1, int[] arr2)
+    {
+        int i = 0, j = 0, k = 0;
+        var merged = new int[arr1.Length + arr2.Length];
+
+        while (i < arr1.Length && j < arr2.Length)
+            if (arr1[i].CompareTo(arr2[j]) < 0)
+                merged[k++] = arr1[i++];
+            else
+                merged[k++] = arr2[j++];
+
+        while (i < arr1.Length) merged[k++] = arr1[i++];
+
+        while (j < arr2.Length) merged[k++] = arr2[j++];
+
+        return merged;
+    }
+
+    public static int[] OddEvenSort(int[] list)
+    {
+        var commun = Communicator.world;
+        var communRank = commun.Rank;
+        var communSize = commun.Size;
+
+        var subListLength = list.Length / communSize + (list.Length % communSize > 0 ? 1 : 0);
+        var subList = new int[subListLength];
+
+        if (communRank == 0)
+            subList = commun.Scatter(list.Chunk(subListLength).ToArray(), 0);
+        else
+            subList = commun.Scatter<int[]>(0);
+
+        Array.Sort(subList);
+
+        for (var phase = 0; phase < communSize; phase++)
+        {
+            if (phase % 2 == communRank % 2 && communRank < communSize - 1)
+            {
+                commun.Send<int[]>(subList, communRank + 1, 0);
+                subList = commun.Receive<int[]>(communRank + 1, 0);
+            }
+            else if (phase % 2 != communRank % 2 && communRank > 0)
+            {
+                var received = commun.Receive<int[]>(communRank - 1, 0);
+                var merged = Merge(received, subList);
+
+                commun.Send<int[]>(merged.Take(subListLength).ToArray(), communRank - 1, 0);
+                subList = merged.Skip(subListLength).ToArray();
+            }
+        }
+
+        if (communRank == 0)
+            list = commun.Gather(subList, 0).SelectMany(x => x).ToArray();
+        else
+            commun.Gather(subList, 0);
+
+        return list;
+    }
+}
